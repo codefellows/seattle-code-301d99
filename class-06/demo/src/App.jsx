665cbccfe8c82d99a2fda07b80f5a3e57588cd58
Audrey Patterson <audrey.patterson31@gmail.com>
2023-08-16 16:55:08 -0700

    Update and rename App.js to App.jsx
    updates demo code file names and structure for Vite

--- class-06/demo/src/App.jsx
+++ class-06/demo/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 class App extends React.Component {
   constructor(props){
     super(props);
@@ -11,7 +13,7 @@ class App extends React.Component {
   }
 
   getLocation = async () => {
-    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+    const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${this.state.searchQuery}&format=json`;
     const res = await axios.get(API);
     console.log(res.data[0])
     this.setState({ location:res.data[0] });
@@ -22,7 +24,7 @@ class App extends React.Component {
       <>
         <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="search for a city"></input>
         <button onClick={this.getLocation}>Explore!</button>
-        {this.state.location.place_id && 
+        {this.state.location.place_id &&
           <h2>The city is: {this.state.location.display_name}</h2>
         }
       </>
